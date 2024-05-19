931a9b26d2b1ca839bdb4829f9857b31af28c6a5
Habibur Rahman <126333479+habi8@users.noreply.github.com>
2024-05-20 00:09:40 +0600

    Add files via upload

--- CSE 401/Lab07_1422.c
+++ CSE 401/Lab07_1422.c
@@ -1,5 +1,6 @@
 #include <pthread.h>
 #include <stdio.h>
+#include<math.h>
 #include <stdlib.h>
 float pointsIn;
 float pointsTotal;
@@ -7,7 +8,6 @@ int number;
 float pie;
 
 void *runner1(void *param);
-void *runner2(void *param);
 
 int main(int argc, char *argv[]){
 
@@ -25,18 +25,17 @@ int main(int argc, char *argv[]){
 	number=atoi(argv[1]);
 	
 	if (atoi(argv[1]) < 0) {
-	fprintf(stderr,"Argument %d must be non-		  negative\n",atoi(argv[1]));
+	fprintf(stderr,"Argument %d must be non-negative\n",atoi(argv[1]));
 	
 	return -1;
 	}
 	pthread_create(&tid1,&attr,runner1,argv[1]);
-	//pthread_create(&tid2,&attr,runner2,*number);
+	
 	
 	pthread_join(tid1,NULL);
-	pthread_join(tid2,NULL);
 	pie=(float)4*((float)pointsIn/(float)pointsTotal);
 	
-	printf("Value of pie is: %lf",pie);
+	printf("Value of pie is: %f",pie);
 }
 
 void *runner1(void *param){
@@ -50,10 +49,8 @@ void *runner1(void *param){
 				random1=((float)rand()/RAND_MAX)*2-1;
 				random2=((float)rand()/RAND_MAX)*2-1;
 				pointsTotal++;
-				if(abs(random1)<1 && abs(random2)<1) pointsIn++;
+				if(sqrt(pow(random1,2)+pow(random2,2))<=1) pointsIn++;
 			}
 }
 
-void *runner2(void *param){
-		
-}
+
